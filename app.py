f969e4593db07269b44a2df58affc31ffdfa48be
charan-o2 <81547870+charan-o2@users.noreply.github.com>
2021-04-18 16:49:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import pandas as  pd
+import requests
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+import numpy as np
 import joblib
 model =joblib.load('Project_Data')
 st.title('Sentiment Analysis')
+st.write(""" # Sentiment Prediction App  """)
+ip = []
 ip = st.text_input('Enter your message')
-op = model.predict([ip])
+op =  model.predict([ip])
 if st.button('predict'):
   st.title(op[0])
